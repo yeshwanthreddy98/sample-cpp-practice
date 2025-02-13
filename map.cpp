17fb8869681677b4aece4b72c008426c89a0b684
yeshwanth_reddy <pingiliyeshwanth_reddy@epam.com>
2025-02-13 17:56:21 +0530

    added new files

---
+++ map.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+  unordered_map<int,int> mp;
+  for(int i=0;i<10;i++){
+    mp[i]++;
+    cout<<mp[i]<<endl;
+  }
+
+
+  cout<<endl;
+  return 0;
+}
