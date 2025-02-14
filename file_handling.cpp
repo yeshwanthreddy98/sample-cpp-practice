93c223beca91203af7de0fe7ff9f2c377fb6d0b2
yeshwanth_reddy <pingiliyeshwanth_reddy@epam.com>
2025-02-14 16:42:17 +0530

    file handling modifications

--- file_handling.cpp
+++ file_handling.cpp
@@ -8,8 +8,11 @@ int main()
     FILE *fp = fopen("test_file.txt","w");
     int a = 10;
     float b = 20.101;
-    fprintf(fp,"this is new file\n");
-    
+    fprintf(fp,"this is new file in test branch\n");
+    fprintf(fp,"%d\n",a);
+    fprintf(fp,"%f\n",b);
+    fclose(fp);
+    cout<<"successfully modified the file"<<endl;    
 
     cout << endl;
     return 0;
