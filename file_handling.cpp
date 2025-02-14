0041d8d9a332d37863b38d3836d997f3a7a47911
yeshwanth_reddy <pingiliyeshwanth_reddy@epam.com>
2025-02-14 15:55:18 +0530

    added file handling

---
+++ file_handling.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+// #include<stdio.h>
+using namespace std;
+
+int main()
+{
+
+    FILE *fp = fopen("test_file.txt","w");
+    int a = 10;
+    float b = 20.101;
+    fprintf(fp,"this is new file\n");
+    
+
+    cout << endl;
+    return 0;
+}
